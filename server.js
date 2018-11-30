a46c0869631e73181134945a2a0cc7ca63a8cd9f
Samantha <samareimorgan@gmail.com>
2018-11-29 18:30:25 -0700

    create server.js and gitignore

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+//Require scraping tools
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+//Require all data models
+var db = require("./models");
+
+//Declare Port 
+var PORT = process.env.PORT|8080;  //process.env.PORT is for heroku
+
+//Get app instance of Express
+var app = require("express");
+
+
+//Configure Middleware
+
+//use morgan to log requests
+app.use(logger("dev"));
+
+//Parse Application Body as JSON
+app.use(express.urlencoded({ extendend: true }));
+app.use(express.json());
+
+//Set up Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handelbars");
+
+
+//
